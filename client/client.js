9efce76c0e705b68bfe0321c68d95de44a1a11de
Subrata Dhal <subrata@ebctechnologies.com>
2026-08-19 01:42:05 +0530

    added the client

--- client/client.js
+++ client/client.js
@@ -1,4 +1,20 @@
 // Paste on the console and connect with the websocket server and send message. See the server how its responding
 // You can see in the network tab and see how the connection is formed up
 const socket = new WebSocket("ws://localhost:8000");
-socket.send("Hi this side is client! Hi Server")
+
+
+socket.onopen = () => {
+    console.log("Connected with the server");
+    socket.send("Hi Server")
+}
+socket.onmessage = (event) => {
+    console.log(`server: ${event.data}`)
+}
+
+socket.onerror = (error) => {
+    console.log(error)
+}
+
+socket.onclose = () => {
+    console.log("connection is closed");
+}
